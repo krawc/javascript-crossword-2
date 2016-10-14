32014f7fbe91172404563556bc9ffde94581bd54
krawc <konradkrawczyk@Konrads-MacBook-Air.local>
2016-10-14 20:30:28 +0800

    lll
    lllll

---
+++ puzzle.js
@@ -0,0 +1,105 @@
+{
+  "title": "Sample Puzzle",
+  "by": "By Line",
+  "clues": [
+    { "d":"A", "n":2, "x":2, "y":0, "a":"PIV", "c":"Pretty in Vogue (Abbr.)" },
+    { "d":"A", "n":5, "x":6, "y":0, "a":"THERAPIST", "c":"“So, I think your issues stem from your parents’ not loving you”, he says" },
+    { "d":"A", "n":13, "x":0, "y":1, "a":"NARNIA", "c":"Where the lion roams deep in the closet" },
+    { "d":"A", "n":15, "x":7, "y":1, "a":"APER", "c":"Imitators of gorillas?" },
+    { "d":"A", "n":16, "x":12, "y":1, "a":"STO", "c":"STOP one letter short" },
+    { "d":"A", "n":17, "x":0, "y":2, "a":"TROVE", "c":"(T)travel, buried treasure" },
+   
+    { "d":"A", "n":18, "x":6, "y":2, "a":"OCAEDITOR", "c":"Arshaun, Savannah, Lathika, e.g." },
+  
+  { "d":"A", "n":20, "x":0, "y":3, "a":"HOMEWORK", "c":"Domestic chores for Rihanna" },
+    
+    { "d":"A", "n":22, "x":11, "y":3, "a":"OMON", "c":"Russian special police service" },
+    
+    { "d":"A", "n":23, "x":0, "y":4, "a":"EMOS", "c":"Mid-2000s angsty cool kids involved in chemical romances" },
+   
+      { "d":"A", "n":24, "x":5, "y":4, "a":"MIA", "c":"Sri Lankan-English rapper who wants to live fast and die young" },
+ 
+    { "d":"A", "n":25, "x":10, "y":4, "a":"AMIPA", "c":"Hoppy brew in the morning" },
+    
+    { "d":"A", "n":26, "x":0, "y":5, "a":"RA", "c":"“__ on duty!”, they say as they’re about to ruin your Friday night" },
+    
+    { "d":"A", "n":27, "x":5, "y":5, "a":"NETHER", "c":"Down these “-lands”" },
+    { "d":"A", "n":29, "x":12, "y":5, "a":"RED", "c":"The color of blood, big Chinese money, and hongbaos" },
+    { "d":"A", "n":31, "x":4, "y":6, "a":"MINH", "c":"Ho Chi ____ or Viet ___" },
+
+
+
+    { "d":"A", "n":32, "x":9, "y":6, "a":"RC", "c":"Living in Jinqiao, this is where you get your mail (Abbr.)" },
+    
+    { "d":"A", "n":33, "x":13, "y":6, "a":"RO", "c":".cn or .de for Romania" },
+    { "d":"A", "n":34, "x":1, "y":7, "a":"TOTO", "c":"This little dog blesses the rains down in Africa" },
+    { "d":"A", "n":36, "x":6, "y":7, "a":"TORAHSASS", "c":"Old Testament donkey, or a rabbi throwing shade, depending on how you read it" },
+    { "d":"A", "n":39, "x":7, "y":8, "a":"FRESHMAN", "c":"Not stale, not female, yet academically multisex" },
+    
+
+
+
+
+    { "d":"A", "n":41, "x":9, "y":8, "a":"SAMI", "c":"Reindeer herders also known as Laplanders, made famous by Linnaeus" },
+    { "d":"A", "n":42, "x":0, "y":9, "a":"IOMA", "c":"A package posted to Iowa and Massachusetts" },
+    { "d":"A", "n":43, "x":0, "y":9, "a":"AT", "c":"Email address punctuation" },
+    { "d":"A", "n":44, "x":8, "y":9, "a":"AMNESIA", "c":"You can’t be blamed for forgetting this common soap opera plot device" },
+    { "d":"A", "n":45, "x":0, "y":10, "a":"FURRY", "c":"Like a stuffed animal, or Brad Pitt’s 2014 tank with an extra “r”" },
+    
+
+
+    { "d":"A", "n":47, "x":6, "y":10, "a":"IAMUGLY", "c":"If you say this to yourself in the mirror, you might need to see a 5A" },
+    { "d":"A", "n":49, "x":0, "y":11, "a":"TBH", "c":"Text-speak before a harsh truth" },
+    { "d":"A", "n":50, "x":4, "y":11, "a":"OYOY", "c":"Throw your yoyo in reverse" },
+    { "d":"A", "n":52, "x":9, "y":11, "a":"SET", "c":"A mean deity of Ancient Egypt, or a collection of objects in math" },
+    { "d":"A", "n":53, "x":13, "y":11, "a":"GO", "c":"Twice a Chinese board game makes a pretty good dancer" },
+    { "d":"A", "n":54, "x":0, "y":12, "a":"ELYSIAN", "c":"Name of the eternal fields for virtuous Greeks" },
+    { "d":"A", "n":57, "x":10, "y":12, "a":"LITON", "c":"It is turned up and the opposite of off" },
+
+    { "d":"A", "n":59, "x":0, "y":13, "a":"EEMILY", "c":"Dickinson or Blunt’s first second, and her first first" },
+    { "d":"A", "n":60, "x":7, "y":13, "a":"TIESTO", "c":"Dutch DJ with a 2015 Grammy for remixing “All of Me”" },
+    { "d":"A", "n":62, "x":2, "y":14, "a":"EULOGIES", "c":"Zoolander is bad at giving these final stories" },
+    { "d":"A", "n":63, "x":11, "y":14, "a":"OPAL", "c":"A gem of a German carmaker, misspelled" },
+
+    { "d":"D", "n":1, "x":0, "y":0, "a":"INTHERE", "c":"Not out here" },
+    { "d":"D", "n":2, "x":2, "y":0, "a":"PROMO", "c":"Advertisement colloquially" },
+    { "d":"D", "n":3, "x":3, "y":0, "a":"INVEST", "c":"Put money in" },
+    { "d":"D", "n":4, "x":4, "y":0, "a":"VIEW", "c":"Singular of Drake’s latest album" },
+    { "d":"D", "n":6, "x":7, "y":0, "a":"HACKATHON", "c":"NYUSH’s premier competition for nerds (other than this one)" },
+    { "d":"D", "n":7, "x":8, "y":0, "a":"EPA", "c":"Environmental Protection Agency (Abbr.)" },
+    { "d":"D", "n":8, "x":9, "y":0, "a":"REAL", "c":"Brazilian currency that is not imaginary" },
+    { "d":"D", "n":9, "x":10, "y":0, "a":"ARD", "c":"Artistic or Ardougne (abbr.)" },
+    { "d":"D", "n":10, "x":12, "y":0, "a":"ISTMIR", "c":"Das ___ ___ egal (German)" },
+    { "d":"D", "n":11, "x":13, "y":0, "a":"STOOPERS", "c":"Porch or sidewalk enthusiasts" },
+    { "d":"D", "n":12, "x":14, "y":0, "a":"TORNADOSEASONAL", "c":"Spinning storm followed by an equally time-sensitive deal" },
+    { "d":"D", "n":14, "x":1, "y":1, "a":"AROMA", "c":"Smell" },
+    { "d":"D", "n":18, "x":6, "y":2, "a":"ORIENTATION", "c":"The week in which you flourished" },
+    { "d":"D", "n":19, "x":11, "y":2, "a":"IOM", "c":"International Organization for Migration (Abbr.)" },
+    { "d":"D", "n":21, "x":5, "y":3, "a":"OMNI", "c":"All in Latin" },
+    { "d":"D", "n":25, "x":0, "y":3, "a":"ARCHANGELS", "c":"Michael, Uriel, Gabriel, etc." },
+    { "d":"D", "n":28, "x":9, "y":5, "a":"ERASMUS", "c":"Dutch renaissance humanist, now also an EU exchange program" },
+    { "d":"D", "n":30, "x":2, "y":6, "a":"POEMRHYME", "c":"“Verses, curses”! What a limerick is and ought to do." },
+    { "d":"D", "n":31, "x":4, "y":6, "a":"MOH", "c":"Popular video game series or the USA’s highest military award" },
+    { "d":"D", "n":34, "x":1, "y":7, "a":"TROUBLE", "c":"I knew this is what you were when we first met" },
+    { "d":"D", "n":35, "x":3, "y":7, "a":"TSAR", "c":"Slavic Caesar" },
+    { "d":"D", "n":37, "x":11, "y":7, "a":"SMELTITO", "c":"Who dealt it in this Japanese city, or an odorous former Yugoslavian strongman preceded by 14D phonetically" },
+    { "d":"D", "n":38, "x":12, "y":7, "a":"AISY", "c":"Morgan Freeman “rove” this flower on her errands" },
+   
+
+    { "d":"D", "n":39, "x":0, "y":8, "a":"FIFTEEN", "c":"The scariest number for a 39A" },
+    { "d":"D", "n":40, "x":5, "y":8, "a":"MA", "c":"Horse, question, or mother" },
+    { "d":"D", "n":44, "x":8, "y":9, "a":"AM", "c":"Ante meridiem in the USA" },
+    { "d":"D", "n":46, "x":4, "y":10, "a":"YOILL", "c":"Slangy “hey” followed by an expression of willingness" },
+   
+    { "d":"D", "n":48, "x":6, "y":9, "a":"AY", "c":"__ lmao [Insert 3 Alien Emojis here]" },
+    { "d":"D", "n":51, "x":10, "y":10, "a":"YAYO", "c":"Street powder, or deceased 50 Cent compatriot" },
+    { "d":"D", "n":53, "x":11, "y":13, "a":"GO", "c":"See 53A" },
+    { "d":"D", "n":55, "x":3, "y":12, "a":"SIU", "c":"Cantonese pork dish with a nice “char”" },
+
+    { "d":"D", "n":56, "x":8, "y":12, "a":"LIE", "c":"What politicians, parents, and advisors do" },
+    { "d":"D", "n":58, "x":12, "y":12, "a":"TOP", "c":"Not bottom" },
+    { "d":"D", "n":60, "x":7, "y":13, "a":"TI", "c":"A very calculating rapper" },
+    { "d":"D", "n":61, "x":9, "y":13, "a":"ES", "c":"How you would end your URL in Madrid" }
+ 
+  ]
+}
